031cc218ba95a25cfbd4c02f4251b5b7d5fe7adf
El Lizok <lizanechyporuk@gmail.com>
2024-06-21 00:26:59 +0300

    DIA imgs and icons:
    -changed icon in header url

--- src/components/Header/components/Header.jsx
+++ src/components/Header/components/Header.jsx
@@ -3,7 +3,11 @@ import styles from "./Header.module.scss";
 function Header() {
   return (
     <header className={styles.header}>
-      <img src={`/icons/shared/logo.svg`} alt="Logo" className={styles.image} />
+      <img
+        src={`${process.env.PUBLIC_URL}/icons/shared/logo.svg`}
+        alt="Logo"
+        className={styles.image}
+      />
       <p className={styles.heading}>Dice it</p>
     </header>
   );
